b6ca678190c9fb89047ff3c2287d15c1700aaaea
weillercarvalho <engweiller@hotmail.com>
2024-05-01 03:13:12 -0300

    feat: Added some services and controllers related with User

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
+import {
+    Controller,
+    Post,
+    Body,
+    Inject,
+  } from '@nestjs/common';
+
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+@Inject()
+private readonly userService: UserService
+    @Post('signup')
+    async signupUser(
+        @Body() userData: Prisma.UserCreateInput,
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
+}
