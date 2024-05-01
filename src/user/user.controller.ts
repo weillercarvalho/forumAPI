d56272a979f6c19e1fdc2ec2df79884049e39aef
weillercarvalho <engweiller@hotmail.com>
2024-05-01 20:53:10 -0300

    feat: Added some initial validations with bcrypt.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,4 +18,14 @@ private readonly userService: UserService
     ): Promise<UserModel> {
         return this.userService.createUser(userData);
     }
+    @Post('signin')
+    async signinUser(
+        @Body() userData: Prisma.UserWhereUniqueInput,
+    ): Promise<UserModel> {
+        try {
+            return this.userService.user(userData);
+        } catch (error) {
+            console.error('Error:', error?.message)
+        }
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,38 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UserService {
     @Inject()
     private readonly prisma: PrismaService
 
-    async user(
-        userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-    ): Promise<User | null> {
-        return this.prisma.user.findUnique({
-            where: userWhereUniqueInput,
+    async user(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<User | null> {
+        const password: string = userWhereUniqueInput.password as string;
+
+        const getUniqueUser = await this.prisma.user.findUnique({
+            where: { email: userWhereUniqueInput.email }
         });
+
+        if (!getUniqueUser) {
+            throw new Error ("User not found!")
+        }
+
+        console.log(getUniqueUser.password);
+        if (await bcrypt.compare(password, getUniqueUser.password)) {
+            return getUniqueUser;
+        } else {
+            throw new Error ("Password doesn't match!")
+        }
     }
 
     async createUser(data: Prisma.UserCreateInput): Promise<User> {
-        return this.prisma.user.create({ data })
+        const saltOrRounds = 10;
+        const { password }: Prisma.UserCreateInput = data;
+        const hashPassword = await bcrypt.hash(password, saltOrRounds);
+        return this.prisma.user.create({ data: { ...data, password: hashPassword } })
     }
 
     async updateUser(params: {
